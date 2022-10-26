c82953b619623a48fa0340584fe7c8d6876fbe80
kentookura <kento@okura.at>
2022-10-26 17:56:36 +0200

    fix formatting

--- src/Main.elm
+++ src/Main.elm
@@ -2,16 +2,20 @@ module Main exposing (..)
 
 import Playground exposing (..)
 
-even : Int -> Bool
-even n =  modBy 2 n == 0
 
+even : Int -> Bool
+even n =
+    modBy 2 n == 0
 
 
 color : Bool -> Color
-color b = case b of 
-    False -> white
-    True -> black
+color b =
+    case b of
+        False ->
+            white
 
+        True ->
+            black
 
 
 board : Int -> Program () Screen ( Int, Int )
@@ -19,11 +23,10 @@ board size =
     picture [ rectangle black 10 10 ]
 
 
-drawBox : (Int, Int) -> Shape
-drawBox (a, b)= rectangle (color (even (a+b))) 10 10
+drawBox : ( Int, Int ) -> Shape
+drawBox ( a, b ) =
+    rectangle (color (even (a + b))) 10 10
 
 
-
-
-
-main = board 5
+main =
+    board 5
